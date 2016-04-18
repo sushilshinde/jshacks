5f4063107a94b97c860ed1dccb6c0eb9b8879697
sushatgithub <shinde.sushil@gmail.com>
2016-04-19 00:07:24 +0530

    Update w3schoolhack.js

--- w3schoolhack.js
+++ w3schoolhack.js
@@ -1,3 +1,19 @@
+/*
+This script is a hack for w3schools.com, as these tutorials are divided into pages, readers needs to to click next for each page.
+This script will load all the pages in the tutorial without refreshing a page.
+Usage steps 
+1.  Open any tutorial from w3schools.com, eg. http://www.w3schools.com/jsref/default.asp
+2.  Open console in Firefox or Chrome (Ctr+Shift+i)
+3.  Paste below line to console where the curson is blikning
+    s=document.createElement('script');s.type='text/javascript';s.src='https://raw.githubusercontent.com/sushatgithub/jshacks/master/w3schoolhack.js'; document.body.appendChild(s);
+*/
+/* Source Code */
+/**
+ * @param {String } removeElements  contain comma seperated ids, classes, tags for jquery selector function 
+ * @param {Number} maxPages maximum number of pages to be fetched
+ * @param {String} nextPageLinkElement id or class of the next page link element
+ * @param {Function} callback process extra logic for each page fetch
+*/
 (function (removeClasses, maxPages, nextButton, callback) {
   (function (cp) {
     var script = document.createElement('script');
@@ -30,13 +46,6 @@
     fetchPage();
   });
 })
-/**
- * Converts tutorial into one page for tutorials like tutorialpoint.com, w3school.com
- * @param {String } removeElements  contain comma seperated ids, classes, tags for jquery selector function 
- * @param {Number} maxPages maximum number of pages to be fetched
- * @return {String} nextPageLinkElement id or class of the next page link element
- * @return {Function} callback process extra logic for each page fetch
-*/
 ('.w3-container,#topnav,#leftmenuinner,#card,#bg-exit,div#google_image_div,div[id*="google"],#leftmenu,iframe,#mainLeaderboard,#right,.footer', 100, '.next a', function () {
   jQuery('#main').css('width', '100%');
   jQuery('#main').css('padding-left', '100px');
